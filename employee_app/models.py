dd236fcc5f5bc982bcf7b63b1342de4285dcba4c
Tinto John <tintojohn980@gmail.com>
2021-07-21 13:43:38 +0530

    defined __str__ function  at Position model

--- employee_app/models.py
+++ employee_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Position(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 class Employee(models.Model):
     Full_Name = models.CharField(max_length=100)
     Mobile_Number = models.CharField(max_length=20)
