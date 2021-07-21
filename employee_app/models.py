d7c476aebf7d45422c20d6af29c7ac400bcd66fb
Tinto John <tintojohn980@gmail.com>
2021-07-21 11:48:35 +0530

    tables Position and Employee were created

--- employee_app/models.py
+++ employee_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    title = models.CharField(max_length=100)
 
+class Employee(models.Model):
+    Full_Name = models.CharField(max_length=100)
+    Mobile_Number = models.CharField(max_length=20)
+    Employee_Code = models.CharField(max_length=10)
+    Position = models.ForeignKey(Position, on_delete=models.CASCADE)
